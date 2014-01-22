e4147e74ccdd5b11f2e573b29c94ceab154b791d
Clement Gorlt <Clement.gorlt@wanadoo.fr>
2014-01-22 14:33:51 +0100

    update du import.sh avec tshark 	modified:   bin/import.sh

--- bin/import.sh
+++ bin/import.sh
@@ -1,1 +1,3 @@
-ls -1 *.pcap | tail -n50 | parallel --gnu 'tcpdump -s0 -A -n -r {1}' | python test.py
+#ls -1 *.pcap | tail -n50 | parallel --gnu 'tcpdump -s0 -A -n -r {1}' | python import.py
+cd sample/pcap/
+ls -1 . | head -n500 | parallel -j 10 --gnu "cat {1} | tshark -E separator=, -E header=yes -Tfields -e http.user_agent -r {1} | python ../../bin/import.py -f {1}"
